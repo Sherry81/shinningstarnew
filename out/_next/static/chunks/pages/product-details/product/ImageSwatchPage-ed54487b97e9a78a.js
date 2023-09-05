49830225e41c2b09a3f36557126086c16fe4c6bf
Sherry81 <shehroze.ali@atex.com>
2023-09-05 23:39:12 +0500

    .

--- out/_next/static/chunks/pages/product-details/product/ImageSwatchPage-ed54487b97e9a78a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6276],{28220:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/product-details/product/ImageSwatchPage",function(){return _(25105)}])}},function(n){n.O(0,[7971,6066,9667,2948,5105,9774,2888,179],(function(){return u=28220,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
